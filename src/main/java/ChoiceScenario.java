17f2b77c0f70510f07d2d00b5f886304858694b7
kevin <kevin.mazure.km@gmail.com>
2021-02-21 15:52:37 +0100

    Correction menu/affichage Scénario

--- src/main/java/ChoiceScenario.java
+++ src/main/java/ChoiceScenario.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ChoiceScenario {
-    public static String tab;
-    static choixScenario s;
+    public static Scenario tab = new Scenario();
+    static scenarioCreator list;
     //fonction qui permet de choisir le scénario
     public static void ChoiceScenario() {
         int i = 0;
@@ -11,20 +12,20 @@ public class ChoiceScenario {
         //affiche tout les scénarios
         System.out.println("You chose the scenario mode.\n");
         System.out.println("Now choose a scenario:");
-        //Initialise le tableau list avec tout les scénario possible
-        s.Initialisation();
-        //récupère la taille du tableau
-        length = s.GetLength();
-        //affiche tout les scénario
-        for (int k = 0; k < length && s.list[k] != null;k++){
-            System.out.println(j+" for "+s.list[k]);
+        //Initialise le tableau test avec tout les scénario possible
+        scenarioCreator test = new scenarioCreator();
+        //enlève le sandbox de la liste des scénario
+        list.scenarioList.remove(0);
+        //affiche la liste des scénario
+        for(Scenario ent : list.scenarioList){
+            System.out.println(j+" for "+ent);
             j++;
         }
         //récupère la valeur que le joueur à écrit dans la console
         Scanner scan2 = new Scanner(System.in);
         i = scan2.nextInt();
         //récupère le titre du scénario choisi par le joueur
-        tab = s.GetList(i);
+        tab = list.scenarioList.get(i-1);
         // game set up
     }
 }
