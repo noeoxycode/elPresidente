8d5c8e9d4bb119552b34d6427da9c0c1c03731c1
nonoxD <noe@marleix.com>
2021-02-23 17:55:37 +0100

    round correction

--- src/main/java/Round.java
+++ src/main/java/Round.java
@@ -1,13 +1,12 @@
 import java.util.Random;
 public class Round {
-    private static ChoiceScenario res;
     public static void Tour(){
         //Choisi un évenement aléatoire
         Random r = new Random();
-        int i = r.nextInt(res.tab.getScenarioEventList().size());
+        int i = r.nextInt(ChoiceScenario.tab.getScenarioEventList().size());
         //affiche l'évenement choisi ci-dessus
-        System.out.println("Event: "+ res.tab.getScenarioEventList().get(i));
+        System.out.println("Event: "+ ChoiceScenario.tab.getScenarioEventList().get(i));
         //affiche les propositions possible
-
+        System.out.println(ChoiceScenario.tab.getScenarioEventList().get(i).getPropositions());
     }
 }
